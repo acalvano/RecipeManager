d84cb90f831b52d4993b8fcf055b4e789b794574
aldocalvano <albertcalvano@gmail.com>
2018-11-18 14:10:05 -0800

    Created the note editor activity

--- app/src/main/java/com/camelsoft/recipemanager/MainActivity.java
+++ app/src/main/java/com/camelsoft/recipemanager/MainActivity.java
@@ -1,8 +1,7 @@
 package com.camelsoft.recipemanager;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,7 +9,6 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.camelsoft.recipemanager.model.RecipeEntity;
 import com.camelsoft.recipemanager.ui.RecipesAdapter;
@@ -21,11 +19,18 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.recycler_view)
     RecyclerView mRecyclerView;
+
+    @OnClick(R.id.fab)
+    void fabClickHandler() {
+        Intent intent = new Intent(this, EditorActivity.class);
+        startActivity(intent);
+    }
 
     private List<RecipeEntity> recipesData = new ArrayList<>();
     private RecipesAdapter mAdapter;
@@ -40,14 +45,6 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         initRecyclerView();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         recipesData.addAll (SampleData.getRecipes());
         for (RecipeEntity recipe:
